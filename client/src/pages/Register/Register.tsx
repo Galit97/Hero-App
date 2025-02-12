37e368c50456dda5f8a8eb3fd5a4b90173c7971d
Galit97 <fitness.galit@gmail.com>
2025-02-12 21:32:14 +0200

    register

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,46 +1,41 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Form from '../Components/Form/Form';
 import FormTitle from '../Components/FormTitle/FormTitle';
 import { useAppVM } from './RegisterVM';
 
-
 function Register() {
   const { registerUser, error } = useAppVM();
-  const [formData, setFormData] = useState({ email: '', fullName: '', password: '' });
   const navigate = useNavigate();
 
   const loginFields = [
-    { name: "email", type: "email" ,placeholder: "Email"},
-    { name: "Full Name", type: "text" ,placeholder: "full name"},
-    { name: "password", type: "password", placeholder: "Password"},
+    { name: "email", type: "email", placeholder: "Email" },
+    { name: "fullName", type: "text", placeholder: "Full Name" },
+    { name: "password", type: "password", placeholder: "Password" },
   ];
-  const handleLogin = (data: Record<string, string>) => {
-    async (data: Record<string, string>) => {
-      console.log("Form Data:", data);
-  
-      
-      const response = await registerUser({
-        email: data.email,
-        fullName: data.fullName,
-        password: data.password,
-      });
-  
-      if (response.success) {
-        alert(response.message);
-        navigate('/login'); 
-      } else {
-        console.error(response.error);
-      }
-  };
 
+  const handleLogin = async (data: Record<string, string>) => {
+    console.log("Form Data:", data);
+
+    const response = await registerUser({
+      email: data.email,
+      fullName: data.fullName,
+      password: data.password,
+    });
+
+    if (response.success) {
+      alert(response.message);
+      navigate('/login');
+    } else {
+      console.error(response.error);
+    }
+  };
 
   return (
     <div>
-      <FormTitle title='Register'></FormTitle>
-      <Form fields={loginFields} buttonLabel='Submit' onSubmit={handleLogin}></Form>
+      <FormTitle title='Register' />
+      <Form fields={loginFields} buttonLabel='Submit' onSubmit={handleLogin} />
     </div>
   );
 }
-}
+
 export default Register;
