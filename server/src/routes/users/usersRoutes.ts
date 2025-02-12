1889fdb146822d0105522ec9bef47920e08b6453
Galit97 <fitness.galit@gmail.com>
2025-02-12 21:28:44 +0200

    user route

--- server/src/routes/users/usersRoutes.ts
+++ server/src/routes/users/usersRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addUser, login, register } from '../../controllers/users/setUsers';
+import { addUser, login, register } from '../../controllers/users/setUserts';
 const router = express.Router();
 
 router.post("/add-user", addUser).post("/register", register).post("/login", login);
